1215d1a70a54a04027a5fbff2bc554e552aa5a7b
Wonters <42609066+Wonters@users.noreply.github.com>
2018-10-11 23:15:52 +0200

    rajout du package matplotlib

--- PlotCsv.py
+++ PlotCsv.py
@@ -5,17 +5,23 @@ import csv
 import os
 import sys
 import string
-#from matplotlib.py import pyplot as plt
-
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.cbook as cbook
 
 print("Entrée le chemin absolue du fichier csv à ploter ")
-PathCsv ='test.csv' #sys.stdin.readline()
+PathCsv ='fichier.csv' #sys.stdin.readline()
 #PathCsv = PathCsv.replace("\n","")
+X,Y=[],[]
 with open(PathCsv, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     print("fichier csv chargé", PathCsv)
     for row in reader:
-        print(row['Y'],row['X'])
-        
-        
+        print(row['X'],row['Y'])  # row est un dictionnaire
+        X.append(row['X'])
+        Y.append(row['Y'])
+print("donnée chargées:\n",X,Y)
+plt.plot(X,Y,'ro')
+plt.show()
+
 
